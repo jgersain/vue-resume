7aa25657053c3d129425264373309f0ab107d6e9
jgersain <jhersein@gmail.com>
2021-05-28 00:35:08 -0500

    Fix missing component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="home">
-    <HelloWorld msg="This will be my homepage!" />
+    Hi
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
